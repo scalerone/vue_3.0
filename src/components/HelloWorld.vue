9ac1c967e3205e03393db489f4b4c61e9ea185df
wuhui <wuhui@yadingdata.com>
2020-05-21 16:59:20 +0800

    computed传入一个 function 函数和对象方式

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,12 +2,16 @@
   <div>
     <h2>ref值：{{ city }}</h2>
     <h3>{{ Msg }}</h3>
+    <h3>转换reactive类型数据{{ userInfo.userName }}</h3>
+    <h3>转换reactive类型数据{{ code }}</h3>
     <button @click="increment">
       Count is: {{ state.count }}, double is: {{ state.double }}
     </button>
     <button @click="getCity">
       你来自哪里？
     </button>
+    请输入年龄:<input type="text" v-model="age" /> <br /><br />
+    我的年龄是:{{ userMsg }}
   </div>
 </template>
 
@@ -40,14 +44,32 @@ export default {
       },
       code: 200
     })
+    const age = ref('')
+    //传入函数的方式
+    const userMsg = computed(() => {
+      return `我的年龄是:` + age.value
+    })
+    //传入对象自定义get set的形式
+    //   const userMsg = computed({
+    //   get(){
+    //     return `computed-obj我的年龄是:`+age.value
+    //   },
+    //   set(val){
+    //     age.value=val;
+    //   }
+    //  })
 
+    console.log(data)
+    console.log(toRefs(data))
     return {
       state,
       city,
       Msg,
       increment,
       getCity,
-      ...toRefs(data)
+      ...toRefs(data),
+      age,
+      userMsg
     }
   }
 }
